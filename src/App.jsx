48a821495ba9e3d0c0a460433de5fb141c9f457c
ZainAsif767 <zainclasher2002@gmail.com>
2023-07-28 08:43:58 +0500

    state and button to toggle

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
 import Carousel from './Components/Carousel/Carousel.jsx';
 import Card from './Components/ProductCard/Card.jsx';
+// import Button from './Components/Button/Button.jsx';
 
 import './App.css';
+import { useState } from 'react';
 
 export default function App() {
+  const [showComponent, setShowComponent] = useState(false);
+
+
+  function handleClick() {
+    setShowComponent((prevShowComponent) => !prevShowComponent);
+  }
   return (
     <div className="App">
-      <Carousel />
-      <Card />
+      <div className="button-container">
+        <button onClick={handleClick}>Toggle</button>
+      </div>
+      <div className="container">
+        {showComponent ? <Card /> : <Carousel />}
+      </div>
+      {/* <Button /> */}
     </div>
   );
 }
