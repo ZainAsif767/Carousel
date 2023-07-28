f728201ff2b67edb8e4cc7c9dea4f56e1ececbdf
ZainAsif767 <zainclasher2002@gmail.com>
2023-07-28 17:23:13 +0500

    Button Rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Carousel from './Components/Carousel/Carousel.jsx';
 import Card from './Components/ProductCard/Card.jsx';
-// import Button from './Components/Button/Button.jsx';
+import Button from './Components/Button/Button.jsx';
 
 import './App.css';
 import { useState } from 'react';
@@ -14,13 +14,13 @@ export default function App() {
   }
   return (
     <div className="App">
-      <div className="button-container">
+      {/* <div className="button-container">
         <button onClick={handleClick}>Toggle</button>
       </div>
       <div className="container">
         {showComponent ? <Card /> : <Carousel />}
-      </div>
-      {/* <Button /> */}
+      </div> */}
+      <Button />
     </div>
   );
 }
--- src/Components/Button/Button.jsx
+++ src/Components/Button/Button.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import './Button.css';
 
 export default function Button() {
     //     return (
